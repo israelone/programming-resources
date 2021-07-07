81b3691af1344a83c0331d0d92e8959be49c48c0
Izzy <itzisraelone@gmail.com>
2021-07-07 13:36:05 -0500

    Added elements and styling to make paragraph collapsible

--- src/components/resource/resource.js
+++ src/components/resource/resource.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  padding-left: 20px;
+  padding: 0 20px;
 `;
 
 const TagsList = styled.ul`
@@ -32,18 +32,56 @@ const Divider = styled.hr`
   width: 90%;
 `;
 
+const ShowMoreButton = styled.button`
+  margin: 10px;
+`;
+
+const DescriptionContainer = styled.div`
+  height: ${(props) =>
+    props.showDescription === false ? props.scrollHeight + 'px' : '50px'};
+  overflow: hidden;
+  transition: height 0.2s ease-out;
+`;
+const DescriptionSection = styled.div``;
+
 const Resource = () => {
+  const [showDescription, setShowDescription] = useState(false);
+  const [scrollHeight, setScrollHeight] = useState(50);
+
+  const showDescriptionHandler = (e) => {
+    console.log(showDescription, scrollHeight, e);
+    setScrollHeight(e.target.previousElementSibling.scrollHeight.toString());
+    setShowDescription(!showDescription);
+  };
+
   return (
     <Container>
       <Header>
         <a href="www.freecodecamp.com">FreeCodeCamp</a>
       </Header>
-      <SubHeader>
-        Our mission: to help people learn to code for free. We accomplish this
-        by creating thousands of videos, articles, and interactive coding
-        lessons - all freely available to the public. We also have thousands of
-        freeCodeCamp study groups around the world.
-      </SubHeader>
+      <SubHeader></SubHeader>
+      <DescriptionSection>
+        <DescriptionContainer
+          showDescription={showDescription}
+          scrollHeight={scrollHeight}
+        >
+          <Description>
+            Our mission: to help people learn to code for free. We accomplish
+            this by creating thousands of videos, articles, and interactive
+            coding lessons - all freely available to the public. We also have
+            thousands of freeCodeCamp study groups around the world. Our
+            mission: to help people learn to code for free. We accomplish this
+            by creating thousands of videos, articles, and interactive coding
+            lessons - all freely available to the public. We also have thousands
+            of freeCodeCamp study groups around the world. Our mission: to help
+            people learn to code for free. We accomplish this by creating
+            thousands of videos, articles, and interactive coding lessons - all
+            freely available to the public. We also have thousands of
+            freeCodeCamp study groups around the world.
+          </Description>
+        </DescriptionContainer>
+        <ShowMoreButton onClick={showDescriptionHandler}>More</ShowMoreButton>
+      </DescriptionSection>
       <Divider></Divider>
       <TagsList>
         <Tag>HTML</Tag>

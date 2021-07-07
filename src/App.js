c817eff45b5c831b087f6488b6c49370b9b6894c
Izzy <itzisraelone@gmail.com>
2021-07-07 13:42:38 -0500

    Added Resources container styling

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import Section from './components/section/section';
 import styled from 'styled-components';
-const ResourcesContaine = styled.div``;
+
+const ResourcesContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  row-gap: 10px;
+`;
 
 function App() {
   return (
     <>
       <h1>Programming Resources</h1>
-      <ResourcesContaine>
+      <ResourcesContainer>
         <Section></Section>
-        <Section></Section><Section></Section>
-      </ResourcesContaine>
+        <Section></Section>
+        <Section></Section>
+      </ResourcesContainer>
     </>
   );
 }
